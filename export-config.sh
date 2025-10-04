986e61e503917f22dde98709816d6c3eb7690732
maudocante <ummominhafilha@gmail.com>
2025-10-04 20:18:15 +0000

    Criar export-config.sh

---
+++ export-config.sh
@@ -0,0 +1,6 @@
+Switch# copy running-config scp:
+Address or name of remote host []? 192.168.1.100
+Destination username []? admin
+Destination filename [c9200L-config]? backup.cfg
+Password: ********
+Writing backup.cfg !!!
